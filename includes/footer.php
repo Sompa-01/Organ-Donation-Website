e9e51f8855fc43242513b9a9b2ca013be88c1602
Coder <sompa.bhui01@gmail.com>
2025-08-31 16:53:04 +0530

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<?php
+// includes/footer.php
+?>
+  </main>
+  <footer>
+    <p>&copy; <?php echo date("Y"); ?> Organ Donation Portal. All rights reserved.</p>
+  </footer>
+  <script src="assets/js/script.js"></script>
+</body>
+</html>
