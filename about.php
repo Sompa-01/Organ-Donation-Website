a8937052bc005280b1342b09cef9c7f219b901e4
Coder <sompa.bhui01@gmail.com>
2025-08-31 16:58:19 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,9 @@
+<?php include("includes/header.php"); ?>
+<h2>About Us</h2>
+<p>
+  This portal is designed to help people pledge their organs for donation and recipients find help. 
+</p>
+<p>
+  We believe organ donation is the gift of life and want to make the process easier and more transparent.
+</p>
+<?php include("includes/footer.php"); ?>
