f654c2f74fa29f3bff357d092887d5e50682ba4f
Coder <sompa.bhui01@gmail.com>
2025-08-31 16:57:52 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include("includes/header.php"); ?>
+<h2>Welcome to the Organ Donation Portal</h2>
+<p>
+  Our mission is to connect organ donors and recipients, and to spread awareness about the importance of organ donation. 
+</p>
+<p>
+  Please register as a donor or recipient to be part of this life-saving initiative.
+</p>
+<a href="pages/register.php" class="btn">Register</a>
+<a href="pages/login.php" class="btn">Login</a>
+<?php include("includes/footer.php"); ?>
